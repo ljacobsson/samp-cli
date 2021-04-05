77a7fb346ce88c3892cedb4d2b31b409933d28c1
ljacobsson <lars@mathem.se>
2021-04-05 21:24:26 +0200

    fix: workaround for jsonpath issue with hyphenated json keys

--- src/commands/import/transformer.js
+++ src/commands/import/transformer.js
@@ -5,7 +5,7 @@ const { Separator } = require("inquirer");
 
 async function transform(template) {
   const metadata = template.Metadata;
-  if (!metadata || !metadata.PatternTransform) {    
+  if (!metadata || !metadata.PatternTransform) {
     return template;
   }
   console.log("Applying transforms...");
@@ -19,8 +19,8 @@ async function placeholderTransforms(template) {
 
   let templateString = JSON.stringify(template);
   for (const property of metadata.PatternTransform.Placeholders || []) {
-    const value = await inputUtil.text(property.Message || 
-      `Placeholder value for ${property.Placeholder}:`
+    const value = await inputUtil.text(
+      property.Message || `Placeholder value for ${property.Placeholder}:`
     );
     templateString = templateString.replaceAll(property.Placeholder, value);
   }
@@ -29,44 +29,47 @@ async function placeholderTransforms(template) {
 async function propertyTransforms(template) {
   const metadata = template.Metadata;
   for (const property of metadata.PatternTransform.Properties || []) {
-    if (!jp.query(template, property.JSONPath).length)
-        continue
-    let defaultValue, value;
-    switch (property.InputType) {
-      case "number":
-        defaultValue = jp.query(template, property.JSONPath);
-        value = parseInt(
-          await inputUtil.text(
-            `${property.Message || "Set value for " + property.JSONPath}:`,
-            defaultValue.length ? defaultValue[0] : undefined
-          )
-        );
+    try {
+      if (!jp.query(template, property.JSONPath).length) continue;
+      let defaultValue, value;
+      switch (property.InputType) {
+        case "number":
+          defaultValue = jp.query(template, property.JSONPath);
+          value = parseInt(
+            await inputUtil.text(
+              `${property.Message || "Set value for " + property.JSONPath}:`,
+              defaultValue.length ? defaultValue[0] : undefined
+            )
+          );
 
-        break;
-      case "string":
-      case "text":
-        defaultValue = jp.query(template, property.JSONPath);
-        value = await inputUtil.text(
-          property.Message,
-          defaultValue.length ? defaultValue[0] : undefined
-        );
-        break;
-      case "runtime-select":
-        value =
-          process.env.SAM_PATTERNS_DEFAULT_RUNTIME ||
-          (await inputUtil.list(
-            `Select Lambda runtime for ${JSONPathToFrieldlyName(
-              property.JSONPath
-            )}.`,
-            [
-              ...runtimes.filter((p) => p.latest),
-              new Separator("*** Older versions ***"),
-              ...runtimes.filter((p) => !p.latest),
-            ]
-          ));
-        break;
+          break;
+        case "string":
+        case "text":
+          defaultValue = jp.query(template, property.JSONPath);
+          value = await inputUtil.text(
+            property.Message,
+            defaultValue.length ? defaultValue[0] : undefined
+          );
+          break;
+        case "runtime-select":
+          value =
+            process.env.SAM_PATTERNS_DEFAULT_RUNTIME ||
+            (await inputUtil.list(
+              `Select Lambda runtime for ${JSONPathToFrieldlyName(
+                property.JSONPath
+              )}.`,
+              [
+                ...runtimes.filter((p) => p.latest),
+                new Separator("*** Older versions ***"),
+                ...runtimes.filter((p) => !p.latest),
+              ]
+            ));
+          break;
+      }
+      if (value) jp.value(template, property.JSONPath, value);
+    } catch (err) {
+      console.log("Could not parse path " + property.JSONPath);
     }
-    if (value) jp.value(template, property.JSONPath, value);
   }
   return template;
 }
