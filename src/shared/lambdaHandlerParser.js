7ff6549d5793dbb525cd9a1360e4190e418c7d79
ljacobsson <lars@mathem.se>
2021-05-28 08:37:19 +0200

    adding missing files

---
+++ src/shared/lambdaHandlerParser.js
@@ -0,0 +1,18 @@
+const { paths } = require("jsonpath");
+const runtimes = require("./runtimes.json");
+
+function buildFileName(globals, props) {
+  const runtime = props.Runtime || globals.Function.Runtime;
+  const extension = runtimes.filter((p) => p.name === runtime)[0].extension;
+  const path = `${(props.CodeUri || globals.Function.CodeUri).replace(
+    /^\W+|\W+$/g,
+    ""
+  )}/${props.Handler.split(".").slice(0, -1).join(".")}${extension}`.trim();
+  return path.replace(
+    /^\//,
+    "");
+}
+
+module.exports = {
+  buildFileName,
+};
