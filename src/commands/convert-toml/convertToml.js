e3e0a30d2441dda03e01587ee8cf5280f82a0e17
ljacobsson <lars@mathem.se>
2023-07-24 09:37:26 +0200

    toml conversion fix

--- src/commands/convert-toml/convertToml.js
+++ src/commands/convert-toml/convertToml.js
@@ -1,15 +1,15 @@
-const ini = require('ini');
+const toml = require('toml');
 const fs = require('fs');
 const path = require('path');
 const inputUtil = require("../../shared/inputUtil");
-const YAML = require('json-to-pretty-yaml');
+const YAML = require('yaml');
 async function run(cmd) {
   if (!fs.existsSync(path.join(process.cwd(), 'samconfig.toml'))) {
     console.log("samconfig.toml not found");
     return;
   }
 
-  const samconfig = ini.parse(fs.readFileSync(path.join(process.cwd(), 'samconfig.toml'), 'utf-8'));
+  const samconfig = toml.parse(fs.readFileSync(path.join(process.cwd(), 'samconfig.toml'), 'utf-8'));
   const samconfigYaml = YAML.stringify(samconfig);
   fs.writeFileSync(path.join(process.cwd(), 'samconfig.yaml'), samconfigYaml);
   deleteToml = await inputUtil.prompt("Conversion complete. Delete samconfig.toml? (Y/n)");
